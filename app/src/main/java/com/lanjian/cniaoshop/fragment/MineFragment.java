2d64b3c0f67081172021b29807c0a77c823d3583
lanjian <819715035@qq.com>
2018-01-23 16:39:36 +0800

    添加购物车模块

--- app/src/main/java/com/lanjian/cniaoshop/fragment/MineFragment.java
+++ app/src/main/java/com/lanjian/cniaoshop/fragment/MineFragment.java
@@ -3,25 +3,52 @@ package com.lanjian.cniaoshop.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.lanjian.cniaoshop.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 /**
  * Created by Ivan on 15/9/22.
  */
-public class MineFragment extends Fragment{
+public class MineFragment extends Fragment {
+
+    @BindView(R.id.profile_image)
+    CircleImageView profileImage;
+    @BindView(R.id.tv_username)
+    TextView tvUsername;
+    @BindView(R.id.tv_my_order)
+    TextView tvMyOrder;
+    @BindView(R.id.tv_favorite)
+    TextView tvFavorite;
+    @BindView(R.id.tv_addr)
+    TextView tvAddr;
+    @BindView(R.id.btn_loginOut)
+    Button btnLoginOut;
+    Unbinder unbinder;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
 
+        View view = inflater.inflate(R.layout.fragment_mine, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
 
-        return  inflater.inflate(R.layout.fragment_mine,container,false);
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
